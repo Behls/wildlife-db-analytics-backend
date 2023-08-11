912ed2e2df14985e0b756fb166f9cf37b570971a
Alanna <58919192+Behls@users.noreply.github.com>
2023-08-11 18:26:07 +0100

    api-getrequests

--- backend/api/api.py
+++ backend/api/api.py
@@ -1,41 +1,159 @@
 
 from flask import Blueprint, jsonify, json
-from ..model import Users
+from ..model import Users, Species, Donation, Patient, Notes, Treatment, Medicine   
 
 api = Blueprint('api', __name__)
-
-@api.route('/patients', methods=['GET'])
-def getAllPatients():
-    something = {"name": "patient1", "date":"dunno"}
-    return something
 
 @api.route('/users', methods=['GET'])
 def getAllUsers():
     getAllUsers = Users.query.all()
-    response = []
-    for user in getAllUsers:
-        response.append(user)
-    
-    return json.dumps(response);
+    data = []
+    response = ""
+    if(getAllUsers):
+        for user in getAllUsers:
+            response = user.serialize()
+            data.append(response)
+            alist = jsonify(data)
+        return alist
+    else:
+        response = {"users": "no_users_found"}
+        return response
 
 @api.route('/users/<int:id>', methods=['GET'])
 def getUserByID(id):
     getUser = Users.query.get(id)
-    # response = {"user:" : getUser.usertype}
-    response = jsonify(getUser.serialize())
-    return response;   
+    if(getUser):
+        response = jsonify(getUser.serialize())
+        return response;   
+    else:
+        response = {"user": "user_not_found"}
+        return response
 
 
+@api.route('/patients/<int:id>', methods=['GET'])
+def getPatientByID(id):
+    getPatient = Patient.query.get(id)
+    if(getPatient):
+        response = jsonify(getPatient.serialize())
+        return response;
+    else:
+        response = {"patient": "patient_not_found"}
+        return response
+    
+@api.route('/patients', methods=['GET'])
+def getAllPatients():
+    getAllPatients = Patient.query.all()
+    data = []
+    response = ""
+    if(getAllPatients):
+        for patient in getAllPatients:
+            response = patient.serialize()
+            data.append(response)
+            alist = jsonify(data)
+        return alist
+    else:
+        response = {"users": "no_patients_found"}
+        return response
+
+@api.route('/species', methods=['GET'])
+def getAllSpecies():
+    getAllSpecies = Species.query.all()
+    data = []
+    response = ""
+    if(getAllSpecies):
+        for species in getAllSpecies:
+            response = species.serialize()
+            data.append(response)
+            alist = jsonify(data)
+        return alist
+    else:
+        response = {"species": "no_species_data_found"}
+        return response
+
+@api.route('/species/<int:id>', methods=['GET'])
+def getSpeciesByID(id):
+    getspecies = Species.query.get(id)
+    if(getspecies):
+        response = jsonify(getspecies.serialize())
+        return response
+    else:
+        response = {"species": "species_not_found"}
+        return response
+
+@api.route('/treatment/<int:id>', methods=['GET'])
+def getTreatmentByID(id):
+    getTreatment = Treatment.query.get(id)
+    if(getTreatment):
+        response = jsonify(getTreatment.serialize())
+        return response
+    else:
+        response = {"treatments": "treatments_not_found"}
+        return response
     
 
-# @api.route('/patients/<id>', methods=['GET'])
-# def getAPatient(id):
-#     pass
+@api.route('/treatments', methods=['GET'])
+def getAllTreatments():
+    getAllTreatments = Treatment.query.all()
+    data = []
+    response = ""
+    if(getAllTreatments):
+        for treatment in getAllTreatments:
+            response = treatment.serialize()
+            data.append(response)
+            alist = jsonify(data)
+        return alist
+    else:
+        response = {"treatments": "no_treatments_data_found"}
+        return response
+    
+@api.route('/donation/<int:id>', methods=['GET'])
+def getDonationsByID(id):
+    getDonations = Donation.query.get(id)
+    if(getDonations):
+        response = jsonify(getDonations.serialize())
+        return response
+    else:
+        response = {"donations": "no_donations_not_found"}
+        return response
+    
 
-# @api.route('/users/<id>', methods=['GET'])
-# def getAUser(id):
-#     pass
+@api.route('/donations', methods=['GET'])
+def getAllDonations():
+    getDonations = Donation.query.all()
+    data = []
+    response = ""
+    if(getDonations):
+        for donation in getDonations:
+            response = donation.serialize()
+            data.append(response)
+            alist = jsonify(data)
+        return alist
+    else:
+        response = {"donations": "no_donations_data_found"}
+        return response
+    
+@api.route('/notes/<int:id>', methods=['GET'])
+def getNotesByID(id):
+    getNotes = Notes.query.get(id)
+    if(getNotes):
+        response = jsonify(getNotes.serialize())
+        return response
+    else:
+        response = {"notes": "notes_not_found"}
+        return response
+    
 
-# @api.route('/species', methods=['GET', 'POST'])
-# def getAllSpecies():
-#     pass
+@api.route('/notes', methods=['GET'])
+def getAllNotes():
+    getNotes = Notes.query.all()
+    data = []
+    response = ""
+    if(getNotes):
+        for note in getNotes:
+            response = note.serialize()
+            data.append(response)
+            alist = jsonify(data)
+        return alist
+    else:
+        response = {"notes": "notes_not_found"}
+        return response
--- backend/model.py
+++ backend/model.py
@@ -11,6 +11,7 @@ from dataclasses import dataclass
 
 @dataclass
 class Users(UserMixin, db.Model):
+    __tablename__ = 'users'
 
     id = db.Column(db.Integer(), primary_key=True, autoincrement=True)
     usertype = db.Column(db.String(255))
@@ -20,7 +21,12 @@ class Users(UserMixin, db.Model):
     address = db.Column(db.String(255))
     password_hash = db.Column(db.String(255))
     contact = db.Column(db.String(255))
-    # user = db.relationship('User', backref='user')
+    organisation = db.Column(db.String(255))
+    treatment = db.relationship('Treatment', backref='users')
+    treatment_id = db.Column(db.Integer(), db.ForeignKey('treatment.id'))
+    notes = db.relationship('Notes', backref='notes')
+    notes_id = db.Column(db.Integer(), db.ForeignKey('notes.id'))
+    
 
     def __init__(self):
         db.create_all()
@@ -36,62 +42,109 @@ class Users(UserMixin, db.Model):
 
     def serialize(self):
         return {"id": self.id,
-                "usertype": self.usertype
+                "usertype": self.usertype,
+                "firstname": self.firstname,
+                "lastname": self.surname,
+                "organisation": self.organisation,
+                "email": self.email,
+                "address": self.address,
+                "contact": self.contact
+                }
+@dataclass    
+class Donation(db.Model):
+    __tablename__ = 'donations'
+    
+    id = db.Column(db.Integer(), primary_key=True, autoincrement=True)
+    type = db.Column(db.String(255))
+    amount = db.Column(db.Float())
+    date = db.Column(db.DateTime(), server_default=func.now())
+
+    def serialize(self):
+        return {"id": self.id,
+                "type": self.type,
+                "amount": self.amount,
+                "date": self.date,
                 }
 
-# class Medicine(db.Model):
-#     __tablename__ = 'medicine'
+@dataclass
+class Medicine(db.Model):
+    __tablename__ = 'medicine'
 
-#     id = db.Column(db.Integer(), primary_key=True, autoincrement=True)
-#     medication_name = db.Column(db.String(255))
-#     dosage = db.Column(db.Integer())
-#     medicine = db.relationship('Medicine', backref='medicine')
+    id = db.Column(db.Integer(), primary_key=True, autoincrement=True)
+    medication_name = db.Column(db.String(255))
+    dosage = db.Column(db.Integer())
+    treatment = db.relationship('Treatment', backref='medicine')
+    treatment_id = db.Column(db.Integer(), db.ForeignKey('treatment.id'))
+    
+    def serialize(self):
+        return {"id": self.id,
+                "medication_name": self.medication_name,
+                "dosage": self.dosage,
+        }
 
-# class Donation(db.Model):
-#     __tablename__ = 'donation'
+@dataclass
+class Notes(db.Model):
+    __tablename__ = 'notes'
 
-#     id = db.Column(db.Integer(), primary_key=True, autoincrement=True)
-#     type = db.Column(db.String(255))
-#     amount = db.Column(db.Float())
-#     date = db.Column(db.DateTime(), server_default=func.now())
+    id = db.Column(db.Integer(), primary_key=True, autoincrement=True)
+    note = db.Column(db.String(255))
+    date_added = db.Column(db.DateTime(), nullable=False, server_default=func.now())
+    added_by = db.Column(db.Integer(), db.ForeignKey('user.id'))
+    treatment = db.relationship('Treatment', backref='notes')
+    
+    def serialize(self):
+        return {"id": self.id,
+                "note": self.note,
+                "date_added": self.date_added,
+                "added_by": self.added_by
+    }
 
-# class Notes(db.Model):
-#     __tablename__ = 'notes'
-
-#     id = db.Column(db.Integer(), primary_key=True, autoincrement=True)
-#     note = db.Column(db.String(255))
-#     date_added = db.Column(db.DateTime(), nullable=False, server_default=func.now())
-#     added_by = db.Column(db.Integer(), db.ForeignKey('user.id'))
-
-
-# class Patient(db.Model):
-#     __tablename__ = 'patient'
-
-#     id = db.Column(db.Integer(), primary_key=True, autoincrement=True)
-#     type = db.Column(db.String(255))
-#     name = db.Column(db.String(255))
-#     weight = db.Column(db.Float())
-#     status = db.Column(db.String(255), nullable=False)
-#     admitted = db.Column(db.DateTime(), nullable=False, server_default=func.now())
-#     # discharge = db.Column(db.DateTime(), server_default=func.now())
-#     patient = db.relationship('Patient', backref='patient')
+@dataclass
+class Patient(db.Model):
+    __tablename__ = 'patients'
 
 
-# class Treatment(db.Model):
-    # __tablename__ = 'treatment'
+    id = db.Column(db.Integer(), primary_key=True, autoincrement=True)
+    species_id = db.Column(db.Integer(), db.ForeignKey('species.id'))
+    name = db.Column(db.String(255))
+    weight = db.Column(db.Float())
+    status = db.Column(db.String(255), nullable=False)
+    admitted = db.Column(db.DateTime(), nullable=False, server_default=func.now())
+    discharge = db.Column(db.String(255))
+    firstname = db.Column(db.String(255))
+    surname = db.Column(db.String(255))
+    email = db.Column(db.String(255), nullable=False)
+    address = db.Column(db.String(255))
+    contact = db.Column(db.String(255))
+    postcode = db.Column(db.String(255))
+    city = db.Column(db.String(255))
+    organisation = db.Column(db.String(255))
+    rescue_note = db.Column(db.String(255))
+    treatment = db.relationship('Treatment', backref='patient')
+    
 
-    # id = db.Column(db.Integer(), primary_key=True, autoincrement=True)
-    # future_treatment = db.Column(db.String(255), nullable=False)
-    # current_treatment = db.Column(db.String(255), nullable=False)
-    # patient_id = db.Column(db.Integer(), db.ForeignKey('patient.id'))
-    # medication_id = db.Column(db.Integer(), db.ForeignKey('medicine.id'))
-    # created_by = db.Column(db.Integer(), db.ForeignKey('user.id'))
-    # note_id = db.Column(db.Integer(), db.ForeignKey('note.id'))
+    def serialize(self):
+            return {"id": self.id,
+                    "species_id": self.species_id,
+                    "name": self.name,
+                    "weight": self.weight,
+                    "status": self.status,
+                    "admitted": self.admitted,
+                    "discharged": self.discharge,
+                    "firstname": self.firstname,
+                    "surname": self.surname,
+                    "email": self.email,
+                    "address": self.address,
+                    "contact": self.contact,
+                    "postcode": self.postcode,
+                    "city": self.city,
+                    "recue_note": self.rescue_note
+            }
 
+
+@dataclass
 class Species(db.Model):
-
-    __tablename__ = "species"
-
+    __tablename__ = 'species'
     id = db.Column(db.Integer(), primary_key=True, autoincrement=True)
     name = db.Column(db.String(255), nullable=False)
     type = db.Column(db.String(255), nullable=False)
@@ -99,3 +152,33 @@ class Species(db.Model):
     def __init__(self, name, type):
         self.name = name
         self.type = type
+
+    def serialize(self):
+            return {"id": self.id,
+                    "name": self.name,
+                    "type": self.type
+            }
+
+
+@dataclass
+class Treatment(db.Model):
+
+    __tablename__ = 'treatments'
+
+    id = db.Column(db.Integer(), primary_key=True, autoincrement=True)
+    future_treatment = db.Column(db.String(255), nullable=False)
+    current_treatment = db.Column(db.String(255), nullable=False)
+    patient_id = db.Column(db.Integer(), db.ForeignKey('patients.id'))
+    medication_id = db.Column(db.Integer(), db.ForeignKey('medicine.id'))
+    created_by = db.Column(db.Integer(), db.ForeignKey('users.id'))
+    note_id = db.Column(db.Integer(), db.ForeignKey('notes.id'))
+
+    def serialize(self):
+            return {"id": self.id,
+                    "future_treatment": self.future_treatment,
+                    "current_treatment": self.current_treatment,
+                    "patient_id": self.patient_id,
+                    "medication_id": self.medication_id,
+                    "created_by": self.created_by,
+                    "note_id": self.note_id,
+                }
